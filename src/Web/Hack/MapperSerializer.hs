0eefb1974d18e35f742367e0fb92ce5e5f0dc210
finnsson <oscar.finnsson@gmail.com>
2009-12-03 21:48:24 +0100

    hlint and refactor serializer.

--- src/Web/Hack/MapperSerializer.hs
+++ src/Web/Hack/MapperSerializer.hs
@@ -7,12 +7,10 @@ serializeToXml :: MapperOutput -> String
 serializeToXml (MapperOutputError msg) = showElement $ element "error" msg
 serializeToXml (MapperOutput xs) = showElement $ elementFold "view" serializeColumn xs
 
-serializeColumn xs = elementFold "row" serializeKV xs
+serializeColumn  = elementFold "row" $ uncurry element 
 
-serializeKV (k,v) = element k v
+elementFold name f  = element' name . map (Elem . f) 
 
-elementFold name f xs = element' name $ map (Elem . f) xs
-
-element k v = Element (QName k Nothing Nothing) [] [Text $ CData CDataText v Nothing] Nothing
+element k v = element' k [Text $ CData CDataText v Nothing] 
 
 element' k v = Element (QName k Nothing Nothing) [] v Nothing
